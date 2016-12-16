9e1771d78da71c395e2ce95132b596d98da6651f
Anthony Cleaver <anthony.cleaver@midlandhr.co.uk>
2016-12-16 15:08:18 +0000

    Adding binary

---
+++ bin.js
@@ -0,0 +1,18 @@
+var index = require('./index');
+
+function getFlags(arguments) {
+    var x,
+        out = {};
+
+    for (x = 0; x < arguments.length; x++) {
+        var arg = arguments[x];
+
+        if (arg.substring(0, 2) === '--') {
+            out[arg.substring(2)] = arguments[++x];
+        }
+    }
+
+    return out;
+}
+
+index(process.argv[process.argv.length - 1], getFlags(process.argv));
